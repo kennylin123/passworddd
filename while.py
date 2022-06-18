29fc0e795d6d01d3f69a861c2fbc7eb25e178932
Kenny <kenny32555@gmail.com>
2022-06-18 18:15:15 +0800

    version change

--- while.py
+++ while.py
@@ -1,11 +1,11 @@
 # password = 'a123456'
+password = 'a123456'
 number = 3
-while True:
-	code = input('plz input user code: ')
-	number -= 1
-	print('還剩下', number ,'次機會')
-	if code == 'a123456':
-		print('登入成功')
+while number > 0:
+	pwd = input('plz input user code: ')
+	if pwd == password:
+		print('login successful')
 		break
-	if number <= 0:
-		break
+	else:
+		number -= 1
+		print(number ,'times remaining')
